73fba4c8e12efb9605ea59418b2faf4f06db4736
sontx <xuanson33bk@gmail.com>
2017-03-14 14:49:51 +0700

    + example project

---
+++ Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using SonTx.Utils.Executor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Example
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ProcessExecutor executor = new ProcessExecutor("notepad.exe");
+            executor.ExecuteAndWaitAsync().Wait();
+        }
+    }
+}
