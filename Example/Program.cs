d1404ca67e02644d5e1db7c4c6bfa9d3f2b0a3a4
sontx <xuanson33bk@gmail.com>
2017-03-15 14:54:47 +0700

    - example for communicate feature

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Sontx.Utils.Executor.Example
 {
@@ -31,10 +28,13 @@ namespace Sontx.Utils.Executor.Example
 
         private static void Main(string[] args)
         {
-            ArgumentDeserializer deserializer = new ArgumentDeserializer();
-            if (deserializer.Deserialize())
+            var clientProcess = new ClientProcess();
+            if (clientProcess.Deserialize())
             {
                 Console.WriteLine("I'm called");
+
+                // receive arguments from caller process
+                var deserializer = clientProcess.Deserializer;
 
                 var person1 = deserializer.GetArgument<Person>("person1");
                 var person2 = deserializer.GetArgument<Person>(1);
@@ -47,18 +47,29 @@ namespace Sontx.Utils.Executor.Example
                 Console.WriteLine(myDog);
                 Console.WriteLine(myString);
                 Console.WriteLine(myCode);
+
+                // communicate with caller process
+                clientProcess.Transmitter.PrepareAsync().Wait();
+                var message = clientProcess.Transmitter.Receive<string>();
+                Console.WriteLine(message);
             }
             else
             {
                 Console.WriteLine("I'm calling");
 
                 ProcessExecutor executor = new ProcessExecutor("Example.exe");// execute itself
+
+                // pass arguments while calling
                 executor.Add("person1", new Person() { Name = "tran xuan son", Age = 23 });
                 executor.Add(new Person() { Name = "tran xuan soan", Age = 24 });
                 executor.Add("myDog", new Dog() { CoatColor = "black", Type = "i don't know" });
                 executor.Add("some string");
                 executor.Add(3393);
                 executor.Execute();
+
+                // communicate with process that is called
+                executor.Transmitter.PrepareAsync().Wait();
+                executor.Transmitter.Send("ok, i am fine");
             }
             Console.Read();
         }
